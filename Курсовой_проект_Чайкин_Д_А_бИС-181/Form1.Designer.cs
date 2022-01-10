528b20bde2ee59495f20c8f5d8e8f6f5de0b859f
DanilaChaikin <97462370+DanilaChaikin@users.noreply.github.com>
2022-01-10 19:21:45 +0300

    Add files via upload

---
+++ Курсовой_проект_Чайкин_Д_А_бИС-181/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Factory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bPeople1 = new System.Windows.Forms.Button();
+            this.statusMachine = new System.Windows.Forms.Label();
+            this.people1 = new System.Windows.Forms.Label();
+            this.loader = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.item = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.item)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bPeople1
+            // 
+            this.bPeople1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bPeople1.Location = new System.Drawing.Point(624, 375);
+            this.bPeople1.Margin = new System.Windows.Forms.Padding(4);
+            this.bPeople1.Name = "bPeople1";
+            this.bPeople1.Size = new System.Drawing.Size(241, 49);
+            this.bPeople1.TabIndex = 4;
+            this.bPeople1.Text = "Создать деталь";
+            this.bPeople1.UseVisualStyleBackColor = true;
+            this.bPeople1.Click += new System.EventHandler(this.newItem);
+            // 
+            // statusMachine
+            // 
+            this.statusMachine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusMachine.Location = new System.Drawing.Point(13, 192);
+            this.statusMachine.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusMachine.Name = "statusMachine";
+            this.statusMachine.Size = new System.Drawing.Size(348, 70);
+            this.statusMachine.TabIndex = 6;
+            this.statusMachine.Text = "Готов";
+            // 
+            // people1
+            // 
+            this.people1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.people1.Location = new System.Drawing.Point(199, 375);
+            this.people1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.people1.Name = "people1";
+            this.people1.Size = new System.Drawing.Size(281, 48);
+            this.people1.TabIndex = 8;
+            this.people1.Text = "Ожидаю";
+            // 
+            // loader
+            // 
+            this.loader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loader.Location = new System.Drawing.Point(603, 192);
+            this.loader.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.loader.Name = "loader";
+            this.loader.Size = new System.Drawing.Size(433, 48);
+            this.loader.TabIndex = 16;
+            this.loader.Text = "Готов";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // item
+            // 
+            this.item.Enabled = false;
+            this.item.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.item.Location = new System.Drawing.Point(185, 104);
+            this.item.Margin = new System.Windows.Forms.Padding(4);
+            this.item.Name = "item";
+            this.item.Size = new System.Drawing.Size(64, 64);
+            this.item.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.item.TabIndex = 19;
+            this.item.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(608, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 261);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(166, 142);
+            this.pictureBox3.TabIndex = 21;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 437);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.item);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.loader);
+            this.Controls.Add(this.people1);
+            this.Controls.Add(this.statusMachine);
+            this.Controls.Add(this.bPeople1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Изготовление детали";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.item)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.Label loader;
+        private System.Windows.Forms.Button bPeople1;
+        private System.Windows.Forms.Label statusMachine;
+        private System.Windows.Forms.Label people1;
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox item;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ Курсовой_проект_Чайкин_Д_А_бИС-181/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+namespace Factory
+{
+    public partial class Form1 : Form
+    {
+        private Machine _auto;
+        private Miller[] _millers;
+        private Loader _loader;
+        private ManualResetEvent pogrNew1;
+        private ManualResetEvent pogrReady;
+        private ManualResetEvent evToLoaderpogr;
+        private ManualResetEvent MachineWork;
+
+        private static Semaphore _pool;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Image.FromFile("C:\\Users\\admin\\Desktop\\WindowsFormsApp2\\Images\\Image1.png");
+            pictureBox2.Image = Image.FromFile("C:\\Users\\admin\\Desktop\\WindowsFormsApp2\\Images\\Image4.png");
+            pictureBox3.Image = Image.FromFile("C:\\Users\\admin\\Desktop\\WindowsFormsApp2\\Images\\Image6.png");
+            item.Image = Image.FromFile("C:\\Users\\admin\\Desktop\\WindowsFormsApp2\\Images\\Image5.png");
+            CheckForIllegalCrossThreadCalls = false;
+            pogrNew1 = new ManualResetEvent(false);
+            pogrReady = new ManualResetEvent(false);
+            MachineWork = new ManualResetEvent(false);
+            evToLoaderpogr = new ManualResetEvent(false);
+            _pool = new Semaphore(1, 1);
+            _millers = new Miller[1];
+            _millers[0] = new Miller(ref people1, _pool, pogrNew1, pogrReady, MachineWork, evToLoaderpogr);
+            _auto = new Machine(ref statusMachine, ref item, MachineWork, pogrReady);
+            _loader = new Loader(ref loader, evToLoaderpogr);
+        }
+
+        private void newItem(object sender, EventArgs e)
+        {
+            Button PushButtom = sender as Button;
+            switch (PushButtom.Text)
+            {
+                case "Создать деталь":
+                    pogrNew1.Set();
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+    interface ILoader
+    {
+        void receivedItem();
+        void loadedItem();
+    }
+    
+    class Miller
+    {
+        public Thread Thrd;
+        ManualResetEvent ItemNew;
+        ManualResetEvent ItemReady;
+        ManualResetEvent MachineWork;
+        ManualResetEvent ItemToLoader;
+ 
+        private Semaphore MachineIsBusy;
+        private Label text;
+
+        public Miller(  ref Label field, 
+                        Semaphore MachineIsBusy, 
+                        ManualResetEvent ItemNew, 
+                        ManualResetEvent ItemReady, 
+                        ManualResetEvent MachineWork, 
+                        ManualResetEvent ItemToLoader  )
+        {
+            text = field;
+            this.MachineIsBusy = MachineIsBusy;
+            this.ItemNew = ItemNew;
+            this.ItemReady = ItemReady;
+            this.MachineWork = MachineWork;
+            this.ItemToLoader = ItemToLoader;
+            
+            Thrd = new Thread(Run);
+            Thrd.Start();
+        }
+
+        void Run()
+        {
+            while (true)
+            {
+                text.Text = "Свободен";
+                ItemNew.WaitOne();              
+                MachineIsBusy.WaitOne();           
+                text.Text = "Деталь принял в работу";
+                MachineWork.Set();
+                ItemReady.WaitOne();
+                text.Text = "Деталь готова";
+                ItemNew.Reset();
+                ItemReady.Reset();
+                MachineIsBusy.Release();
+                ItemToLoader.Set();
+            }
+        }
+    }
+
+    class Loader : ILoader
+    {
+        public Thread Thrd;
+        public ManualResetEvent evToLoader;
+        public Label text;
+        
+        public Loader(ref Label field, ManualResetEvent evToLoader)
+        {
+            Thrd = new Thread(this.Run);
+            this.evToLoader = evToLoader;
+            text = field;
+            Thrd.Start();
+        }
+        
+        public void Run()
+        {
+            while (true)
+            {
+                evToLoader.WaitOne();
+                receivedItem();
+                Thread.Sleep(1000);
+                loadedItem();
+                evToLoader.Reset();
+            }
+        }
+        
+        public void receivedItem()
+        {
+            text.Text = "Погрузка детали";
+        }
+
+        public void loadedItem()
+        {
+            text.Text = "Деталь отгружена";
+        }
+    }
+    class Machine 
+    {
+        public Thread Thrd;
+        public ManualResetEvent MachineIsBusy;
+        public ManualResetEvent ItemReady;
+        public Label text;
+        public PictureBox Thing;
+
+        public Machine(ref Label field, ref PictureBox Thing, ManualResetEvent MachineIsBusy, ManualResetEvent ItemReady)
+        {
+            Thrd = new Thread(Run);
+            this.MachineIsBusy = MachineIsBusy;
+            this.ItemReady = ItemReady;
+            this.Thing = Thing;
+            text = field;
+            Thrd.Start();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                
+                MachineIsBusy.WaitOne();
+                int start = 210;
+                int end = 400;
+                int Y = 81;
+                int X = start;
+                int step = 1;
+                Thing.Location = new Point(210, 81);
+                text.Text = "Станок изготавливает деталь";
+                Thread.Sleep(5000);
+                text.Text = "Деталь готова";
+               
+                while (X < end) 
+                {
+                    Thing.Location = new Point(X, Y);
+                    X += step;
+                    Thread.Sleep(5);
+                }
+                MachineIsBusy.Reset();
+                ItemReady.Set();
+            }
+        }      
+    }
+}
